f6e5aebb6036ac447c542c4580c412cd84b538c7
Trevor Wheeler <sendtrevmail@gmail.com>
2019-03-10 12:00:50 +1000

    initial webpack integration

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+// const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('webpack-cleanup-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist',
+    },
+    // devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { sourceMap: true } },
+                    { loader: 'postcss-loader', options: { sourceMap: true } },
+                    { loader: 'sass-loader', options: { sourceMap: true } }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    plugins: [
+
+        new CleanWebpackPlugin(['dist']),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/views/index.html',
+            filename: 'index.html',
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 8085,
+            proxy: 'http://localhost:8080/'
+        }, {
+                reload: false
+            }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+        }),
+        new CopyWebpackPlugin([{
+            from: 'src/img',
+            to: 'img'
+        }]),
+
+    ]
+};
